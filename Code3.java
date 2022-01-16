2929daab725a14465c09d75d06599f47b479f753
sangyeop oh <yeo1125@naver.com>
2022-01-16 21:55:16 +0900

    study

---
+++ Code3.java
@@ -0,0 +1,9 @@
+
+public class Code3 {
+
+	public static void main(String[] args) {
+	System.out.println("'Hello'");
+
+	}
+
+}
---
+++ Code4.java
@@ -0,0 +1,9 @@
+
+public class Code4 {
+
+	public static void main(String[] args) {
+	System.out.println("\"Hello World\"");
+
+	}
+
+}
